5ed0575be3187d12d874fa8d485965375a927d47
RyanEphgrave <Ryan2065@Gmail.com>
2023-04-24 17:14:41 -0500

    Updated deploy script

--- AutomatedControls/DeployToAzureAutomation.ps1
+++ AutomatedControls/DeployToAzureAutomation.ps1
@@ -1,4 +1,4 @@
-
+$VerbosePreference = 'Continue'
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module Az.Automation -Force -Confirm:$false
 
@@ -9,7 +9,20 @@ $RunbookLocation = [System.IO.Path]::Join($PSScriptRoot, "Runbooks")
 
 $Runbooks = Get-ChildItem -Path $RunbookLocation -Recurse -Include *.ps1 -File
 
+$AllAzureRunbooks = Get-AzAutomationRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -ErrorAction SilentlyContinue
+$ExportedRunbookFolder = [System.IO.Path]::Join($PSScriptRoot, "bin", "ExportedRunbooks")
+
 # Import runbooks to Azure
 foreach ($Runbook in $Runbooks) {
-    Import-AzAutomationRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Path $Runbook.FullName -Type PowerShell -Force -Published
+    Export-AzAutomationRunbook -Name $Runbook.BaseName -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -OutputFolder $ExportedRunbookFolder -Force -ErrorAction SilentlyContinue
+    $ExportedRunbook = [System.IO.Path]::Join($ExportedRunbookFolder, $Runbook.Name)
+    if(Test-Path $ExportedRunbook) {
+        $ExportedRunbookContent = Get-Content $ExportedRunbook -Raw
+        $GitRunbookContent = Get-Content $Runbook.FullName -Raw
+        if($ExportedRunbookContent -eq $GitRunbookContent) {
+            Write-Verbose "Runbook $($Runbook.BaseName) already exists in Azure and is up to date"
+            continue
+        }
+    }
+    $null = Import-AzAutomationRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Path $Runbook.FullName -Type PowerShell -Force -Published
 }
