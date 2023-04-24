e4898815f5cd7fb181354819c9d301db0695736d
RyanEphgrave <Ryan2065@Gmail.com>
2023-04-24 17:28:05 -0500

    updated script to remove old runbooks

--- AutomatedControls/DeployToAzureAutomation.ps1
+++ AutomatedControls/DeployToAzureAutomation.ps1
@@ -1,16 +1,22 @@
 $VerbosePreference = 'Continue'
+
+#Install Az.Automation module
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module Az.Automation -Force -Confirm:$false
 
+# Set ResourceGroup and Automation Account Name
 $ResourceGroupName = "AutomationAccountRG"
 $AutomationAccountName = "AutomationAccount"
 
+# Set paths we'll need
 $RunbookLocation = [System.IO.Path]::Join($PSScriptRoot, "Runbooks")
+$ExportedRunbookFolder = [System.IO.Path]::Join($PSScriptRoot, "bin", "ExportedRunbooks")
 
+# Get all runbooks in this repository
 $Runbooks = Get-ChildItem -Path $RunbookLocation -Recurse -Include *.ps1 -File
 
+# Get all runbooks in Azure
 $AllAzureRunbooks = Get-AzAutomationRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -ErrorAction SilentlyContinue
-$ExportedRunbookFolder = [System.IO.Path]::Join($PSScriptRoot, "bin", "ExportedRunbooks")
 
 $null = New-Item -ItemType Directory -Path $ExportedRunbookFolder -Force
 # Import runbooks to Azure
@@ -25,12 +31,20 @@ foreach ($Runbook in $Runbooks) {
             continue
         }
         else{
-            Write-Verbose "Runbook $($Runbook.BaseName) already exists in Azure but is out of date - Will import"
+            Write-Output "Runbook $($Runbook.BaseName) already exists in Azure but is out of date - Will import"
         }
     }
     else{
-        Write-Verbose "Runbook $($Runbook.BaseName) does not exist in Azure - Will import"
-        (Get-ChildItem $ExportedRunbookFolder -Recurse).FullName
+        Write-Output "Runbook $($Runbook.BaseName) does not exist in Azure - Will import"
     }
     $null = Import-AzAutomationRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Path $Runbook.FullName -Type PowerShell -Force -Published
+}
+
+Foreach($AzureRunbook in $AllAzureRunbooks){
+    $RunbookName = $AzureRunbook.Name
+    $Runbook = $Runbooks | Where-Object {$_.BaseName -eq $RunbookName}
+    if($null -eq $Runbook){
+        Write-Output "Runbook $($RunbookName) exists in Azure but not in this repository - Will delete"
+        $null = Remove-AzAutomationRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $RunbookName -Force
+    }
 }
